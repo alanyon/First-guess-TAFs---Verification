0a63d5c299a90260578604bfe44eab7271ab5ed8
Andre Lanyon <andre.lanyon@metoffice.gov.uk>
2025-10-16 11:11:00 +0100

    Switching

--- bust_verification/verify_busts.sh
+++ bust_verification/verify_busts.sh
@@ -9,4 +9,4 @@ conda activate default_clone
 export PYTHONPATH=$PYTHONPATH:~andre.lanyon/python 
 export PYTHONPATH=$PYTHONPATH:/home/users/andre.lanyon/first_guess_tafs/First-guess-TAFs---Verification/taf_monitor
 
-python verify_busts.py no > output.txt 2>&1 &
+python verify_busts.py yes > output.txt 2>&1 &
