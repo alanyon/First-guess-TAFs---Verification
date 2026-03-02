c52da45a8fbdb05d9c8d894b7549c51f31b38fd4
Andre Lanyon <andre.lanyon@metoffice.gov.uk>
2026-03-02 16:57:04 +0000

    Switching

--- standard_verification/setup_constants.sh
+++ standard_verification/setup_constants.sh
@@ -1,13 +1,14 @@
 # This script sets up the constants for the verification process.
 
 # For decoding and verification of TAFs
-VER_DATES=20230805-20251204_ml
+VER_DATES=20230805-20260123_ml
 export DATA_DIR=/data/users/andre.lanyon/tafs/verification/${VER_DATES}
 export DECODE_DIR=${DATA_DIR}/decodes
-export TAF_TYPES="no_all Manual"
-export TAF_TYPES_SHORT="au ma"
+export TAF_TYPES="opt_all pes_all Manual"
+export TAF_TYPES_SHORT="op pe ma"
 export COMBS="auma"
-export PLOT_TITLES='{"au": "Auto TAFs",
+export PLOT_TITLES='{"op": "Auto TAFs (Optimistic)",
+                     "pe": "Auto TAFs (Pessimistic)",
                      "ma": "Manual TAFs"}'
 export PYTHONPATH=~clare.bysouth/VerPy/stable
 export ORACLE_OWNER=oracle
@@ -31,10 +32,10 @@ export TAF_9HR="EGHH EGSY EGNJ EGAC EGAE EGBJ EGCK EGEC EGEO EGHI EGNV EGPE \
                 EGNO EGPA EGPB EGPC EGPI EGPL EGPN EGPU EGSC EGSH EGTC EGTK \
                 EGHQ"
 export VERIF_START=20230805
-export VERIF_END=20251205
+export VERIF_END=20260123
 MONTHS="202308 202309 202310 202311 202312 202401 202402 202403 202404 202405 \
         202406 202407 202408 202409 202410 202411 202412 202501 202502 202503 \
-        202504 202505 202506 202507 202508 202509 202510 202511 202512"
+        202504 202505 202506 202507 202508 202509 202510 202511 202512 202601"
 # Factor to address that in ML work, test data is 0.25 * full dataset 
 # (set to 1 otherwise)
 export ML_FACTOR=1
